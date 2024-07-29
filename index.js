521b183acaa5b388e2b6b7e010c9df3f58447328
naumaanansari <nomaanansari1115@gmail.com>
2024-07-29 19:57:30 +0530

    testing

--- index.js
+++ index.js
@@ -49,11 +49,6 @@ server.post('/webhook', express.raw({type: 'application/json'}), async (request,
 
   // Handle the event
   switch (event.type) {
-    case 'payment_intent.created':
-      const paymentIntentCreated = event.data.object;
-      console.log('PaymentIntent was created!', paymentIntentCreated);
-      // Add your business logic here
-      break;
     case 'payment_intent.succeeded':
       const paymentIntentSucceeded = event.data.object;
       const order =await Order.findById(paymentIntentSucceeded.metadata.orderId);
